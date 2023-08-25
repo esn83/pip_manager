b32cb7c22858c162fdeb8ca0e9283f574279074a
esn83 <72857208+esn83@users.noreply.github.com>
2023-08-25 12:25:15 +0200

    initial commit

--- py_pip_manager.py
+++ py_pip_manager.py
@@ -14,15 +14,27 @@ def get_outdated():
 	#subprocess.check_call([sys.executable, "-m", "pip", "list", "--outdated"])
 	outdated = subprocess.check_output([sys.executable, "-m", "pip", "list", "--outdated"], shell=False)	
 	outdated_str = outdated.decode('utf-8')
-	for x in outdated_str.split('\n'):
-		print(x)
+	
+	count = 0
+	for line in outdated_str.split('\n'):
+		print(str(count),line)
+		count += 1
+
+	outdated_split = outdated_str.split('\n')
+	outdated_split = outdated_split[2:] # remove first 2 lines
+	outdated_split = outdated_split[:-1] # remove last empty line
+	for line in outdated_split:
+		print('line : ',line)
+		line_split = line.split(' ')
+		print('line_split : ',line_split)
+		print('package name? :', line_split[0]) # <- is getting the package name like this working?
 
 def update_package(package):
 	subprocess.check_call([sys.executable, "-m", "pip", "install", package, "--upgrade"])
 
 def update_all_packages(package_list):
 	for package in package_list:
-		print('Updating package : '+package)
+		print('updating package : '+package)
 		update_package(package)
 		print()
 
