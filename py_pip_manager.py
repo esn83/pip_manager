f87f1f51c80582e729858e271e0ca8f940849c78
esn83 <72857208+esn83@users.noreply.github.com>
2023-08-28 09:49:50 +0200

    initial commit

--- py_pip_manager.py
+++ py_pip_manager.py
@@ -3,7 +3,7 @@ import sys
 import os
 from datetime import datetime
 
-class py_pyp_manager():
+class py_pip_manager():
 
 	def __init__(self):
 		self.outdated_package_list = []
@@ -25,23 +25,23 @@ class py_pyp_manager():
 		outdated = subprocess.check_output([sys.executable, "-m", "pip", "list", "--outdated"], shell=False)	
 		outdated_str = outdated.decode('utf-8')
 		
-		count = 0
-		for line in outdated_str.split('\n'):
-			print(str(count),line)
-			count += 1
-
 		outdated_split = outdated_str.split('\n')
+		
+		print('Outdated packages')
+		print('--------------------------------')
+		for line in outdated_split:
+			print(line)
+		print('--------------------------------')
+
 		outdated_split = outdated_split[2:] # remove first 2 lines
 		outdated_split = outdated_split[:-1] # remove last 1 empty line
 		for line in outdated_split:
-			print('line :',line)
-			line_split = line.split(' ')
-			print('line_split :',line_split)
-			print('package name? :', line_split[0]) # <- is getting the package name like this working?
-			self.outdated_package_list.append(line_split[0])
+			package = line.split(' ')[0]
+			self.outdated_package_list.append(package)
 
+		print('self.outdated_package_list:')
 		for package in self.outdated_package_list:
-			print('self.outdated_package_list :', package)
+			print(package)
 
 	def update_package(self, package):
 		#subprocess.check_call([sys.executable, "-m", "pip", "install", package, "--upgrade"])
@@ -50,29 +50,29 @@ class py_pyp_manager():
 		# write to log
 		updated_str = updated.decode('utf-8')
 		today = datetime.today().strftime('%Y-%m-%d')
-		self.log = today + '\n'
-		self.log = updated_str + '\n\n'
+		self.log = today + '\n' + updated_str + '\n\n'
 		script_dir = os.path.dirname(__file__) # <-- absolute dir the script is in
 		rel_path = "log.txt"
 		abs_file_path = os.path.join(script_dir, rel_path)
 		with open(abs_file_path, 'a') as log:
-    		log.write(self.log)
+			log.write(self.log)
 
 	def update_all_packages(self, package_list):
-		for package in package_list:
+		self.get_outdated() # updates self.outdated_package_list
+		for package in self.outdated_package_list:
 			print('updating package :',package)
 			self.update_package(package)
 			print()
 
 # --------------------------------------
 
-ppm = py_pyp_manager() # class instance
+ppm = py_pip_manager() # class instance
 
 #ppm.update_pip()
 
 #ppm.get_installed_packages()
 
-#ppm.get_outdated()
+ppm.get_outdated()
 
 #ppm.update_all_packages(package_list)
 
